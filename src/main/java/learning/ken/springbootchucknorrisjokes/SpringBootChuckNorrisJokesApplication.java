4269047c0950403755d66bf8dae3501fa6b9bc55
Ken McKenzie <kenneth.mckenzie@myacuity.com>
2023-02-16 08:38:44 -0500

    Initial commit

---
+++ src/main/java/learning/ken/springbootchucknorrisjokes/SpringBootChuckNorrisJokesApplication.java
@@ -0,0 +1,13 @@
+package learning.ken.springbootchucknorrisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChuckNorrisJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChuckNorrisJokesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learning/ken/springbootchucknorrisjokes/SpringBootChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.ken.springbootchucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChuckNorrisJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
